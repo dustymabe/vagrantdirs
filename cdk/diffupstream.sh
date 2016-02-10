153de223134c7dba0e6106732c4d151cbc1f553d
Dusty Mabe <dusty@dustymabe.com>
2016-02-10 15:02:14 -0500

    CDK: Add in mechanism to diff Vagrant files with cdk.zip

---
+++ cdk/diffupstream.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eux
+
+unzip -p cdk.zip cdk/components/rhel/rhel-ose/Vagrantfile > /tmp/vf.openshift
+vimdiff /tmp/vf.openshift ./Vagrantfile.openshift
+
+unzip -p cdk.zip cdk/components/rhel/rhel-k8s-singlenode-setup/Vagrantfile > /tmp/vf.kubernetes
+vimdiff /tmp/vf.kubernetes ./Vagrantfile.kubernetes
