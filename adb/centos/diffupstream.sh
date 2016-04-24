c8b893a6479105d4f91153d5f1b4194a383fa2cb
Dusty Mabe <dusty@dustymabe.com>
2016-04-23 22:18:38 -0400

    ADB: Sync Vagrantfiles with upstream

--- adb/centos/diffupstream.sh
+++ adb/centos/diffupstream.sh
@@ -6,3 +6,6 @@ vimdiff ./Vagrantfile.openshift /tmp/vf.openshift
 
 curl -L -o /tmp/vf.kubernetes https://github.com/projectatomic/adb-atomic-developer-bundle/raw/master/components/centos/centos-k8s-singlenode-setup/Vagrantfile
 vimdiff ./Vagrantfile.kubernetes /tmp/vf.kubernetes
+
+curl -L -o /tmp/vf.docker https://raw.githubusercontent.com/projectatomic/adb-atomic-developer-bundle/master/components/centos/centos-docker-base-setup/Vagrantfile
+vimdiff ./Vagrantfile.docker /tmp/vf.docker
