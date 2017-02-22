b7411bb9909a91261d7726687fe28a2463b10479
Dusty Mabe <dusty@dustymabe.com>
2017-02-21 19:24:44 -0500

    add in some experimental aws/digital ocean boxes

---
+++ DO/c7/boxfiles/createbox.sh
@@ -0,0 +1,4 @@
+
+# internals of the box file format:
+#https://www.vagrantup.com/docs/boxes/format.html
+tar -czf centos7-digitalocean.box metadata.json Vagrantfile
---
+++ DO/f23/boxfiles/createbox.sh
@@ -0,0 +1,4 @@
+
+# internals of the box file format:
+#https://www.vagrantup.com/docs/boxes/format.html
+tar -czf fedora-digitalocean.box metadata.json Vagrantfile
