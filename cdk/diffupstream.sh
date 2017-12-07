406faf4c1e6f52ae1428de5ba772389c015c6536
Dusty Mabe <dusty@dustymabe.com>
2017-12-07 10:31:30 -0500

    get rid of adb/cdk
    cdk doesn't use vagrant any longer

--- cdk/diffupstream.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eux
-
-unzip -p cdk.zip cdk/components/rhel/rhel-ose/Vagrantfile > /tmp/vf.openshift
-vimdiff /tmp/vf.openshift ./Vagrantfile.openshift
-
-unzip -p cdk.zip cdk/components/rhel/misc/rhel-k8s-singlenode-setup/Vagrantfile > /tmp/vf.kubernetes
-vimdiff /tmp/vf.kubernetes ./Vagrantfile.kubernetes
